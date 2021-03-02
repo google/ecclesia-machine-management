8d49c9b3a266efda5d37db4ff0be32fa90ee385c
Anuraag Bharadwaj <anuraagb@google.com>
2021-03-02 15:18:16 -0800

    Internal Change.
    PiperOrigin-RevId: 360526506

--- ecclesia/magent/redfish/common/update_service.h
+++ ecclesia/magent/redfish/common/update_service.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-#ifndef ECCLESIA_MAGENT_REDFISH_INDUS_UPDATE_SERVICE_H_
-#define ECCLESIA_MAGENT_REDFISH_INDUS_UPDATE_SERVICE_H_
+#ifndef ECCLESIA_MAGENT_REDFISH_COMMON_UPDATE_SERVICE_H_
+#define ECCLESIA_MAGENT_REDFISH_COMMON_UPDATE_SERVICE_H_
 
 #include <string>
 
@@ -50,4 +50,4 @@ class UpdateService : public Resource {
 };
 }  // namespace ecclesia
 
-#endif  // ECCLESIA_MAGENT_REDFISH_INDUS_UPDATE_SERVICE_H_
+#endif  // ECCLESIA_MAGENT_REDFISH_COMMON_UPDATE_SERVICE_H_
