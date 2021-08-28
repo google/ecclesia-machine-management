2943cddbe43b92e14a905c85dcac9092a51a3f40
John Admanski <jadmanski@google.com>
2021-08-27 17:12:33 -0700

    Internal change
    PiperOrigin-RevId: 393457319

--- ecclesia/lib/time/clock_fake.h
+++ ecclesia/lib/time/clock_fake.h
@@ -21,7 +21,6 @@
 #ifndef ECCLESIA_LIB_TIME_CLOCK_FAKE_H_
 #define ECCLESIA_LIB_TIME_CLOCK_FAKE_H_
 
-#include <functional>
 #include "absl/time/clock.h"
 #include "absl/time/time.h"
 #include "ecclesia/lib/time/clock.h"
@@ -33,36 +32,29 @@ class FakeClock : public Clock {
   // Construct a fake clock. It can be initialized to a specific time, or if
   // not specified it will default to (the real) Now.
   FakeClock() : FakeClock(absl::Now()) {}
-  FakeClock(std::function<void(absl::Duration)> sleep) :
-    time_(absl::Now()), sleep_(sleep) {}
-  FakeClock(absl::Time now, std::function<void(absl::Duration)> sleep) :
-    time_(now), sleep_(sleep) {}
   explicit FakeClock(absl::Time now) : time_(now) {}
+
+  FakeClock(const FakeClock &) = delete;
+  FakeClock &operator=(const FakeClock &) = delete;
 
   absl::Time Now() const override { return time_; }
 
   void Sleep(absl::Duration d) override {
-    if (sleep_.has_value()) {
-      // Call the provided sleep function
-      (*sleep_)(d);
-    } else {
-      // Otherwise use default clock-like behavior
-      SleepFor(d);
-    }
+    AdvanceTime(d);
+    SleepCallback(d);
   }
-
-  void SleepFor(absl::Duration d) { AdvanceTime(d); }
 
   // Move time forward by duration. This cannot be used to move time back.
   void AdvanceTime(absl::Duration duration) { time_ += duration; }
 
  private:
+  // A function which will be called on every sleep. The default implementation
+  // does nothing but subclasses can override this to add additional behavior on
+  // sleep, or to do additional checks in a test context.
+  virtual void SleepCallback(absl::Duration d) {}
+
   // The current time this clock holds, can be provided at construction
   absl::Time time_;
-
-  // An optional sleep function that can be provided during construction
-  // will be used if provided otherwise SleepFor is used
-  const absl::optional<std::function<void(absl::Duration)>> sleep_;
 };
 
 }  // namespace ecclesia
