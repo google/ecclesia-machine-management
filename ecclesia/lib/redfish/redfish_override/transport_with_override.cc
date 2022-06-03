a6c06780ab3fc986b290b460235c41a36e157853
Ecclesia Team <ecclesia-robot@google.com>
2022-06-03 10:57:33 -0700

    Internal change
    PiperOrigin-RevId: 452800246

--- ecclesia/lib/redfish/redfish_override/transport_with_override.cc
+++ ecclesia/lib/redfish/redfish_override/transport_with_override.cc
@@ -16,18 +16,269 @@
 
 #include "ecclesia/lib/redfish/redfish_override/transport_with_override.h"
 
+#include <string>
+
+#include "google/protobuf/struct.pb.h"
+#include "absl/container/flat_hash_map.h"
 #include "absl/status/status.h"
-#include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
-#include "absl/synchronization/mutex.h"
+#include "ecclesia/lib/logging/logging.h"
+#include "ecclesia/lib/redfish/redfish_override/rf_override.pb.h"
 #include "ecclesia/lib/redfish/transport/interface.h"
+#include "ecclesia/lib/redfish/transport/struct_proto_conversion.h"
+#include "single_include/nlohmann/json.hpp"
+#include "re2/re2.h"
 
 namespace ecclesia {
+namespace {
+
+using OverrideValue = OverrideField::OverrideValue;
+using IndividualObjectIdentifier = ObjectIdentifier::IndividualObjectIdentifier;
+
+absl::Status JsonReplace(nlohmann::json &json,
+                         const OverrideValue &override_value,
+                         RedfishTransport *transport) {
+  if (override_value.has_value()) {
+    nlohmann::json replace_json = ValueToJson(override_value.value());
+    if (json.type() != replace_json.type()) {
+      return absl::InvalidArgumentError(
+          "Value type is different from original type");
+    }
+    json = replace_json;
+    return absl::OkStatus();
+  } else if (override_value.has_override_by_reading()) {
+    return absl::UnimplementedError("To be implemented");
+  }
+  return absl::InvalidArgumentError("Replace Json not found.");
+}
+
+absl::Status JsonAdd(nlohmann::json &json,
+                     const IndividualObjectIdentifier &object_identifier,
+                     const OverrideValue &override_value,
+                     RedfishTransport *transport) {
+  nlohmann::json add_json;
+  if (override_value.has_value()) {
+    add_json = ValueToJson(override_value.value());
+  } else if (override_value.has_override_by_reading()) {
+    return absl::UnimplementedError("To be implemented");
+  } else {
+    return absl::InvalidArgumentError("Added Json not found.");
+  }
+  if (object_identifier.has_field_name()) {
+    if (json.find(object_identifier.field_name()) != json.end()) {
+      if (json[object_identifier.field_name()].is_array()) {
+        json[object_identifier.field_name()].push_back(add_json);
+        return absl::OkStatus();
+      }
+      return absl::InvalidArgumentError("Json field already exist.");
+    }
+    json[object_identifier.field_name()] = add_json;
+    return absl::OkStatus();
+  } else if (object_identifier.has_array_field()) {
+    return absl::InvalidArgumentError(
+              "Please specify a field before adding inside an array type "
+              "json");
+  } else if (object_identifier.has_array_idx()) {
+    if (!json.is_array()) {
+      return absl::InvalidArgumentError("Json is not an array type");
+    }
+    if (object_identifier.array_idx() >= json.size()) {
+      return absl::InvalidArgumentError("index is out of array range");
+    }
+    if (!json[object_identifier.array_idx()].is_array()) {
+      return absl::InvalidArgumentError(
+          "Please specify a field before adding inside an array type "
+          "json");
+    }
+    json[object_identifier.array_idx()].push_back(add_json);
+    return absl::OkStatus();
+  }
+  return absl::InvalidArgumentError("Added Json not found.");
+}
+
+absl::Status JsonClear(nlohmann::json &json,
+                       const IndividualObjectIdentifier &object_identifier) {
+  if (object_identifier.has_field_name()) {
+    json.erase(json.find(object_identifier.field_name()));
+    return absl::OkStatus();
+  } else if (object_identifier.has_array_field()) {
+    if (!json.is_array()) {
+      return absl::InvalidArgumentError("Json is not an array type");
+    }
+    for (int i = 0; i < json.size(); i++) {
+      if (json[i].contains(object_identifier.array_field().field_name()) &&
+          json[i][object_identifier.array_field().field_name()] ==
+              ValueToJson(object_identifier.array_field().value())) {
+        json.erase(i);
+        return absl::OkStatus();
+      }
+    }
+  } else if (object_identifier.has_array_idx()) {
+    if (!json.is_array()) {
+      return absl::InvalidArgumentError("Json is not an array type");
+    }
+    if (object_identifier.array_idx() >= json.size()) {
+      return absl::InvalidArgumentError("index is out of array range");
+    }
+    json.erase(object_identifier.array_idx());
+    return absl::OkStatus();
+  }
+  return absl::InvalidArgumentError("Cleared Json not found.");
+}
+
+// Recursively find the target sub-JSON object in a JSON object till, then call
+// the action functions (JsonReplace, JsonAdd, JsonClear). The idx is the index
+// to do recursive find with object_identifier
+absl::Status FindObjectAndAct(nlohmann::json &json,
+                               const ObjectIdentifier &object_identifier,
+                               int idx, const OverrideValue &override_value,
+                               ecclesia::RedfishTransport *transport,
+                               OverrideField::ActionCase action) {
+  if (action == OverrideField::ActionCase::kActionReplace &&
+      idx == object_identifier.individual_object_identifier_size()) {
+    return JsonReplace(json, override_value, transport);
+  }
+  if (action == OverrideField::ActionCase::kActionAdd &&
+      idx == object_identifier.individual_object_identifier_size() - 1) {
+    return JsonAdd(json,
+                   object_identifier.individual_object_identifier().Get(idx),
+                   override_value, transport);
+  }
+  if (action == OverrideField::ActionCase::kActionClear &&
+      idx == object_identifier.individual_object_identifier_size() - 1) {
+    return JsonClear(json,
+                     object_identifier.individual_object_identifier().Get(idx));
+  }
+  if (object_identifier.individual_object_identifier()
+          .Get(idx)
+          .has_field_name()) {
+    if (json.find(object_identifier.individual_object_identifier()
+                      .Get(idx)
+                      .field_name()) != json.end()) {
+      return FindObjectAndAct(
+          json[object_identifier.individual_object_identifier()
+                   .Get(idx)
+                   .field_name()],
+          object_identifier, idx + 1, override_value, transport, action);
+    }
+  } else if (object_identifier.individual_object_identifier()
+                 .Get(idx)
+                 .has_array_field()) {
+    if (!json.is_array()) {
+      return absl::InvalidArgumentError("Json is not an array type");
+    }
+    for (auto &iter : json) {
+      if (iter.contains(object_identifier.individual_object_identifier()
+                            .Get(idx)
+                            .array_field()
+                            .field_name()) &&
+          iter[object_identifier.individual_object_identifier()
+                   .Get(idx)
+                   .array_field()
+                   .field_name()] ==
+              ValueToJson(object_identifier.individual_object_identifier()
+                              .Get(idx)
+                              .array_field()
+                              .value())) {
+        return FindObjectAndAct(iter, object_identifier, idx + 1,
+                                 override_value, transport, action);
+      }
+    }
+  } else if (object_identifier.individual_object_identifier()
+                 .Get(idx)
+                 .has_array_idx()) {
+    if (!json.is_array()) {
+      return absl::InvalidArgumentError("Json is not an array type");
+    }
+    if (object_identifier.individual_object_identifier().Get(idx).array_idx() >=
+        json.size()) {
+      return absl::InvalidArgumentError("index is out of array range");
+    }
+    return FindObjectAndAct(
+        json[object_identifier.individual_object_identifier()
+                 .Get(idx)
+                 .array_idx()],
+        object_identifier, idx + 1, override_value, transport, action);
+  }
+  return absl::InvalidArgumentError("Required Json not found.");
+}
+// Updating the result by the specific OverrideField
+absl::Status ResultUpdateHelper(const OverrideField &field,
+                          RedfishTransport::Result &result,
+                          RedfishTransport *transport) {
+  switch (field.Action_case()) {
+    case OverrideField::kActionReplace: {
+      auto result_check = FindObjectAndAct(
+          result.body, field.action_replace().object_identifier(), 0,
+          field.action_replace().override_value(), transport,
+          OverrideField::ActionCase::kActionReplace);
+      if (!result_check.ok()) {
+        return result_check;
+      }
+      break;
+    }
+    case OverrideField::kActionAdd: {
+      auto result_check =
+          FindObjectAndAct(result.body, field.action_add().object_identifier(),
+                            0, field.action_add().override_value(), transport,
+                            OverrideField::ActionCase::kActionAdd);
+      if (!result_check.ok()) {
+        return result_check;
+      }
+      break;
+    }
+    case OverrideField::kActionClear: {
+      auto result_check = FindObjectAndAct(
+          result.body, field.action_clear().object_identifier(), 0,
+          field.action_add().override_value(), transport,
+          OverrideField::ActionCase::kActionClear);
+      if (!result_check.ok()) {
+        return result_check;
+      }
+      break;
+    }
+    default:
+      return absl::InvalidArgumentError("No action specified");
+  }
+  return absl::OkStatus();
+}
+}  // namespace
 
 absl::StatusOr<RedfishTransport::Result> RedfishTransportWithOverride::Get(
     absl::string_view path) {
+  auto get_result = redfish_transport_->Get(path);
+  if (!get_result.ok()) {
+    return get_result;
+  }
   absl::MutexLock mu(&policy_lock_);
-  return absl::UnimplementedError("To be implemented");
+  auto iter = override_policy_.override_content_map_uri().find(path);
+  if (iter != override_policy_.override_content_map_uri().end()) {
+    for (const auto &field : iter->second.override_field()) {
+      auto update_status =
+          ResultUpdateHelper(field, *get_result, redfish_transport_.get());
+      if (!update_status.ok()) {
+        WarningLog() << absl::StrFormat(
+            "Failed to perform override to uri: %s, failure: %s.", path,
+            update_status.message());
+      }
+    }
+  }
+  for (const auto &[uri_regex, override_content] :
+       override_policy_.override_content_map_regex()) {
+    if (!RE2::FullMatch(path, uri_regex)) {
+      continue;
+    }
+    for (const auto &field : override_content.override_field()) {
+      auto update_status =
+          ResultUpdateHelper(field, *get_result, redfish_transport_.get());
+      if (!update_status.ok()) {
+        WarningLog() << absl::StrFormat(
+            "Failed to perform override to uri: %s, failure: %s.", path,
+            update_status.message());
+      }
+    }
+  }
+  return *get_result;
 }
 
 }  // namespace ecclesia
---
+++ ecclesia/lib/redfish/redfish_override/transport_with_override_test.cc
@@ -0,0 +1,534 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ecclesia/lib/redfish/redfish_override/transport_with_override.h"
+
+#include <memory>
+#include <utility>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/strings/string_view.h"
+#include "ecclesia/lib/protobuf/parse.h"
+#include "ecclesia/lib/redfish/transport/interface.h"
+#include "ecclesia/lib/redfish/transport/mocked_interface.h"
+#include "ecclesia/lib/testing/status.h"
+
+namespace ecclesia {
+namespace {
+using ::testing::Eq;
+using ::testing::Return;
+
+class RedfishOverrideTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    transport_ = std::make_unique<RedfishTransportMock>();
+    transport_mock_ptr_ = transport_.get();
+    expected_result1_ = nlohmann::json::parse(R"json({
+    "TestString": "test123",
+    "TestNumber": 123.0,
+    "TestBool": true,
+    "TestArray":[
+      {
+        "TestStruct": "tests0"
+      },
+      {
+        "TestStruct": "tests1"
+      },
+      {
+        "TestStruct": "tests2"
+      },
+      {
+        "TestNumber": 1234.0 
+      },
+      "TestArrayString",
+      [ "TestArrayInArray1" ]
+    ]})json",
+                                              nullptr, false);
+    expected_result2_ = nlohmann::json::parse(R"json({
+    "TestString": "test456",
+    "TestNumber": 456.0,
+    "TestBool": false,
+    "TestArray":[
+      56789.0,
+      {
+        "TestStruct":"tests0"
+      },
+      {
+        "TestStruct":"tests1"
+      },
+      {
+        "TestStruct":"tests2"
+      },
+      {
+        "TestNumber": 4567.0 
+      },
+      "TestArrayString2",
+      [ "TestArrayInArray2" ]
+    ]})json",
+                                              nullptr, false);
+    absl::string_view uri1 = "/expected/result/1";
+    absl::string_view uri2 = "/expected/result/2";
+    EXPECT_CALL(*transport_mock_ptr_, Get(uri1))
+        .WillRepeatedly(Return(
+            RedfishTransport::Result{.code = 200, .body = expected_result1_}));
+    EXPECT_CALL(*transport_mock_ptr_, Get(uri2))
+        .WillRepeatedly(Return(
+            RedfishTransport::Result{.code = 200, .body = expected_result2_}));
+  }
+  std::unique_ptr<RedfishTransportMock> transport_;
+  RedfishTransportMock *transport_mock_ptr_;
+  nlohmann::json expected_result1_;
+  nlohmann::json expected_result2_;
+};
+
+TEST_F(RedfishOverrideTest, GetReplaceValue) {
+  OverridePolicy policy = ParseTextProtoOrDie(R"pb(
+    override_content_map_uri: {
+      key: "/expected/result/1"
+      value: {
+        override_field:
+        [ {
+          action_replace: {
+            object_identifier: {
+              individual_object_identifier:
+              [ { field_name: "TestString" }]
+            }
+            override_value: {
+              value: { string_value: "OverrideReplaceByField" }
+            }
+          }
+        }
+          , {
+            action_replace: {
+              object_identifier: {
+                individual_object_identifier:
+                [ { field_name: "TestArray" }
+                  , {
+                    array_field: {
+                      field_name: "TestNumber"
+                      value: { number_value: 1234 }
+                    }
+                  }
+                  , { field_name: "TestNumber" }]
+              }
+              override_value: { value: { number_value: 54321 } }
+            }
+          }
+          , {
+            action_replace: {
+              object_identifier: {
+                individual_object_identifier:
+                [ { field_name: "TestArray" }
+                  , { array_idx: 0 }
+                  , { field_name: "TestStruct" }]
+              }
+              override_value: {
+                value: { string_value: "OverrideReplaceByIndex" }
+              }
+            }
+          }]
+      }
+    }
+  )pb");
+  auto rf_override = std::make_unique<RedfishTransportWithOverride>(
+      std::move(transport_), policy);
+
+  absl::string_view expected_get_str = R"json({
+    "TestString": "OverrideReplaceByField",
+    "TestNumber": 123.0,
+    "TestBool": true,
+    "TestArray":[
+      {"TestStruct":"OverrideReplaceByIndex"},
+      {
+        "TestStruct":"tests1"
+      },
+      {
+        "TestStruct": "tests2"
+      },
+      {
+        "TestNumber": 54321.0 
+      },
+      "TestArrayString",
+      [ "TestArrayInArray1" ]
+      
+    ]})json";
+  nlohmann::json expected_get =
+      nlohmann::json::parse(expected_get_str, nullptr, false);
+  absl::StatusOr<RedfishTransport::Result> res_get =
+      rf_override->Get("/expected/result/1");
+  ASSERT_THAT(res_get, IsOk());
+  EXPECT_THAT(res_get->body, Eq(expected_get));
+  EXPECT_THAT(res_get->code, Eq(200));
+}
+
+TEST_F(RedfishOverrideTest, GetReplaceValueRegex) {
+  OverridePolicy policy = ParseTextProtoOrDie(R"pb(
+    override_content_map_regex: {
+      key: "/expected/(.*)/1"
+      value: {
+        override_field:
+        [ {
+          action_replace: {
+            object_identifier: {
+              individual_object_identifier:
+              [ { field_name: "TestString" }]
+            }
+            override_value: {
+              value: { string_value: "OverrideReplaceByField" }
+            }
+          }
+        }]
+      }
+    }
+  )pb");
+  auto rf_override = std::make_unique<RedfishTransportWithOverride>(
+      std::move(transport_), policy);
+
+  absl::string_view expected_get_str = R"json({
+    "TestString": "OverrideReplaceByField",
+    "TestNumber": 123.0,
+    "TestBool": true,
+    "TestArray":[
+      {
+        "TestStruct": "tests0"
+      },
+      {
+        "TestStruct":"tests1"
+      },
+      {
+        "TestStruct": "tests2"
+      },
+      {
+        "TestNumber": 1234.0 
+      },
+      "TestArrayString",
+      [ "TestArrayInArray1" ]
+
+    ]})json";
+  nlohmann::json expected_get =
+      nlohmann::json::parse(expected_get_str, nullptr, false);
+  absl::StatusOr<RedfishTransport::Result> res_get =
+      rf_override->Get("/expected/result/1");
+  ASSERT_THAT(res_get, IsOk());
+  EXPECT_THAT(res_get->body, Eq(expected_get));
+  EXPECT_THAT(res_get->code, Eq(200));
+}
+
+TEST_F(RedfishOverrideTest, GetAddValue) {
+  OverridePolicy policy = ParseTextProtoOrDie(R"pb(
+    override_content_map_uri: {
+      key: "/expected/result/1"
+      value: {
+        override_field:
+        [ {
+          action_add: {
+            object_identifier: {
+              individual_object_identifier:
+              [ { field_name: "AddField" }]
+            }
+            override_value: { value: { string_value: "OverrideAdd" } }
+          }
+        }
+          , {
+            action_add: {
+              object_identifier: {
+                individual_object_identifier:
+                [ { field_name: "TestArray" }]
+              }
+              override_value: {
+                value: {
+                  struct_value: {
+                    fields {
+                      key: "TestAddArrayString"
+                      value: { string_value: "tests1" }
+                    }
+                    fields {
+                      key: "TestAddArrayNumber"
+                      value: { number_value: 98765.0 }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          , {
+            action_add: {
+              object_identifier: {
+                individual_object_identifier:
+                [ { field_name: "TestArray" }
+                  , { array_idx: 1 }
+                  , { field_name: "AddField" }]
+              }
+              override_value: { value: { string_value: "AddValue" } }
+            }
+          }
+          , {
+            action_add: {
+              object_identifier: {
+                individual_object_identifier:
+                [ { field_name: "TestArray" }
+                  , {
+                    array_field: {
+                      field_name: "TestStruct"
+                      value: { string_value: "tests2" }
+                    }
+                  }
+                  , { field_name: "AddField" }]
+              }
+              override_value: { value: { string_value: "AddValue" } }
+            }
+          }
+          , {
+            action_add: {
+              object_identifier: {
+                individual_object_identifier:
+                [ { field_name: "TestArray" }
+                  , { array_idx: 5 }]
+              }
+              override_value: { value: { string_value: "AddValue" } }
+            }
+          }]
+      }
+    }
+  )pb");
+  auto rf_override = std::make_unique<RedfishTransportWithOverride>(
+      std::move(transport_), policy);
+  absl::string_view expected_get_str = R"json({
+    "TestArray":[
+      {
+        "TestStruct": "tests0"
+      },
+      {
+        "AddField":"AddValue",
+        "TestStruct":"tests1"
+      },
+      {
+        "TestStruct": "tests2",
+        "AddField":"AddValue"
+      },
+      {
+
+        "TestNumber":1234.0
+      },
+      "TestArrayString",
+      ["TestArrayInArray1", "AddValue"],
+      {
+        "TestAddArrayNumber":98765.0,
+        "TestAddArrayString":"tests1"
+      }
+    ],
+    "TestBool":true,
+    "TestNumber":123.0,
+    "TestString":"test123",
+    "AddField":"OverrideAdd"
+  })json";
+  nlohmann::json expected_get =
+      nlohmann::json::parse(expected_get_str, nullptr, false);
+  absl::StatusOr<RedfishTransport::Result> res_get =
+      rf_override->Get("/expected/result/1");
+  ASSERT_THAT(res_get, IsOk());
+  EXPECT_THAT(res_get->body, Eq(expected_get));
+  EXPECT_THAT(res_get->code, Eq(200));
+}
+
+TEST_F(RedfishOverrideTest, GetClear) {
+  OverridePolicy policy = ParseTextProtoOrDie(R"pb(
+    override_content_map_uri: {
+      key: "/expected/result/2"
+      value: {
+        override_field:
+        [ {
+          action_clear: {
+            object_identifier: {
+              individual_object_identifier:
+              [ { field_name: "TestString" }]
+            }
+          }
+        }
+          , {
+            action_clear: {
+              object_identifier: {
+                individual_object_identifier:
+                [ { field_name: "TestArray" }
+                  , {
+                    array_field: {
+                      field_name: "TestStruct"
+                      value: { string_value: "tests2" }
+                    }
+                  }]
+              }
+            }
+          }
+          , {
+            action_clear: {
+              object_identifier: {
+                individual_object_identifier:
+                [ { field_name: "TestArray" }
+                  , { array_idx: 0 }]
+              }
+            }
+          }]
+      }
+    }
+  )pb");
+  auto rf_override = std::make_unique<RedfishTransportWithOverride>(
+      std::move(transport_), policy);
+  absl::string_view expected_get_str = R"json({
+    "TestArray":[
+      {"TestStruct":"tests0"},
+      {"TestStruct":"tests1"},
+      {"TestNumber":4567.0},
+      "TestArrayString2",
+      ["TestArrayInArray2"]
+    ],
+    "TestBool":false,
+    "TestNumber":456.0
+  })json";
+  nlohmann::json expected_get =
+      nlohmann::json::parse(expected_get_str, nullptr, false);
+  absl::StatusOr<RedfishTransport::Result> res_get =
+      rf_override->Get("/expected/result/2");
+  ASSERT_THAT(res_get, IsOk());
+  EXPECT_THAT(res_get->body, Eq(expected_get));
+  EXPECT_THAT(res_get->code, Eq(200));
+}
+TEST_F(RedfishOverrideTest, ReplaceTypeFail) {
+  OverridePolicy replace_policy = ParseTextProtoOrDie(R"pb(
+    override_content_map_uri: {
+      key: "/expected/result/1"
+      value: {
+        override_field:
+        [ {
+          action_replace: {
+            object_identifier: {
+              individual_object_identifier:
+              [ { field_name: "TestString" }]
+            }
+            override_value: { value: { number_value: 1.0 } }
+          }
+        }]
+      }
+    }
+  )pb");
+  auto rf_override = std::make_unique<RedfishTransportWithOverride>(
+      std::move(transport_), replace_policy);
+
+  absl::StatusOr<RedfishTransport::Result> res_get =
+      rf_override->Get("/expected/result/1");
+  ASSERT_THAT(res_get, IsOk());
+}
+
+TEST_F(RedfishOverrideTest, AddArrayFail) {
+  OverridePolicy policy = ParseTextProtoOrDie(R"pb(
+    override_content_map_uri: {
+      key: "/expected/result/1"
+      value: {
+        override_field:
+        [ {
+          action_add: {
+            object_identifier: {
+              individual_object_identifier:
+              [ { field_name: "TestArray" }
+                , { array_idx: 0 }]
+            }
+            override_value: { value: { string_value: "test" } }
+          }
+        }]
+      }
+    }
+  )pb");
+  auto rf_override = std::make_unique<RedfishTransportWithOverride>(
+      std::move(transport_), policy);
+
+  absl::StatusOr<RedfishTransport::Result> res_get =
+      rf_override->Get("/expected/result/1");
+  ASSERT_THAT(res_get, IsOk());
+
+  policy = ParseTextProtoOrDie(R"pb(
+    override_content_map_uri: {
+      key: "/expected/result/1"
+      value: {
+        override_field:
+        [ {
+          action_add: {
+            object_identifier: {
+              individual_object_identifier:
+              [ { field_name: "TestArray" }
+                , {
+                  array_field: {
+                    field_name: "TestStruct"
+                    value { string_value: "tests1" }
+                  }
+                }]
+            }
+            override_value: { value: { string_value: "test" } }
+          }
+        }]
+      }
+    }
+  )pb");
+  rf_override->ReloadOverridePolicy(policy);
+  res_get = rf_override->Get("/expected/result/1");
+  ASSERT_THAT(res_get, IsOk());
+}
+
+TEST_F(RedfishOverrideTest, EmptyIdentifierFail) {
+  OverridePolicy policy = ParseTextProtoOrDie(R"pb(
+    override_content_map_uri: {
+      key: "/expected/result/1"
+      value: {
+        override_field:
+        [ {
+          action_add: {
+            object_identifier: {
+              individual_object_identifier:
+              [ {}]
+            }
+            override_value: { value: { string_value: "test" } }
+          }
+        }]
+      }
+    }
+  )pb");
+  auto rf_override = std::make_unique<RedfishTransportWithOverride>(
+      std::move(transport_), policy);
+
+  absl::StatusOr<RedfishTransport::Result> res_get =
+      rf_override->Get("/expected/result/1");
+  ASSERT_THAT(res_get, IsOk());
+  policy = ParseTextProtoOrDie(R"pb(
+    override_content_map_uri: {
+      key: "/expected/result/1"
+      value: {
+        override_field:
+        [ {
+          action_clear: {
+            object_identifier: {
+              individual_object_identifier:
+              [ {}]
+            }
+          }
+        }]
+      }
+    }
+  )pb");
+  rf_override->ReloadOverridePolicy(policy);
+  res_get = rf_override->Get("/expected/result/1");
+  ASSERT_THAT(res_get, IsOk());
+}
+}  // namespace
+}  // namespace ecclesia
