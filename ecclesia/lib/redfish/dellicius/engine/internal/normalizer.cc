da65d21dc86a80f6e105ce421b2a6a65c91cc37c
Ecclesia Team <ecclesia-robot@google.com>
2023-10-17 12:48:16 -0700

    Adding ability to query array properties in QE.
    PiperOrigin-RevId: 574237122

--- ecclesia/lib/redfish/dellicius/engine/internal/normalizer.cc
+++ ecclesia/lib/redfish/dellicius/engine/internal/normalizer.cc
@@ -28,6 +28,7 @@
 #include "google/protobuf/descriptor.pb.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/str_replace.h"
 #include "absl/strings/string_view.h"
 #include "absl/time/time.h"
@@ -38,6 +39,7 @@
 #include "ecclesia/lib/redfish/interface.h"
 #include "ecclesia/lib/status/macros.h"
 #include "ecclesia/lib/time/proto.h"
+#include "single_include/nlohmann/json.hpp"
 
 namespace ecclesia {
 
@@ -71,6 +73,76 @@ GetAdditionalProperties() {
   return result;
 }
 
+absl::Status GetCollectionPropertyFromRedfishObject(
+    const RedfishObject &redfish_object,
+    const DelliciusQuery::Subquery::RedfishProperty &property,
+    const nlohmann::json &json_obj, SubqueryDataSet::Property &property_out) {
+  if (!json_obj.is_array()) {
+    return absl::InvalidArgumentError(
+        absl::StrCat("Tried to get array property from non array json object: ",
+                     json_obj.dump()));
+  }
+  switch (property.type()) {
+    case DelliciusQuery::Subquery::RedfishProperty::STRING: {
+      for (const std::string &value :
+           json_obj.get<std::vector<std::string>>()) {
+        property_out.mutable_collection_value()
+            ->add_values()
+            ->set_string_value(value);
+      }
+      break;
+    }
+
+    case DelliciusQuery::Subquery::RedfishProperty::BOOLEAN: {
+      for (const bool value : json_obj.get<std::vector<bool>>()) {
+        property_out.mutable_collection_value()
+            ->add_values()
+            ->set_boolean_value(value);
+      }
+      break;
+    }
+    case DelliciusQuery::Subquery::RedfishProperty::DOUBLE: {
+      for (const double value : json_obj.get<std::vector<double>>()) {
+        property_out.mutable_collection_value()
+            ->add_values()
+            ->set_double_value(value);
+      }
+      break;
+    }
+    case DelliciusQuery::Subquery::RedfishProperty::INT64: {
+      for (const int64_t value : json_obj.get<std::vector<int64_t>>()) {
+        property_out.mutable_collection_value()
+            ->add_values()
+            ->set_int64_value(value);
+      }
+      break;
+    }
+    case DelliciusQuery::Subquery::RedfishProperty::DATE_TIME_OFFSET: {
+      for (const auto &json_value : json_obj) {
+        if (!json_value.is_string()) {
+          break;
+        }
+        absl::Time timevalue;
+        if (absl::ParseTime("%Y-%m-%dT%H:%M:%S%Z",
+                            json_value.get<std::string>(), &timevalue,
+                            nullptr)) {
+          absl::StatusOr<google::protobuf::Timestamp> timestamp =
+              AbslTimeToProtoTime(timevalue);
+          if (timestamp.ok()) {
+            *property_out.mutable_collection_value()
+                 ->add_values()
+                 ->mutable_timestamp_value() = *timestamp;
+          }
+        }
+      }
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+  return absl::OkStatus();
+}
 absl::StatusOr<SubqueryDataSet::Property> GetPropertyFromRedfishObject(
     const RedfishObject &redfish_object,
     const DelliciusQuery::Subquery::RedfishProperty &property) {
@@ -85,51 +157,59 @@ absl::StatusOr<SubqueryDataSet::Property> GetPropertyFromRedfishObject(
   using RedfishProperty = DelliciusQuery::Subquery::RedfishProperty;
 
   SubqueryDataSet::Property property_out;
-  switch (property.type()) {
-    case RedfishProperty::STRING: {
-      if (json_obj.is_string()) {
-        property_out.set_string_value(json_obj.get<std::string>());
-      }
-      break;
-    }
-    case RedfishProperty::BOOLEAN: {
-      if (json_obj.is_boolean()) {
-        property_out.set_boolean_value(json_obj.get<bool>());
-      }
-      break;
-    }
-    case RedfishProperty::DOUBLE: {
-      if (json_obj.is_number()) {
-        property_out.set_double_value(json_obj.get<double>());
-      }
-      break;
-    }
-    case RedfishProperty::INT64: {
-      if (json_obj.is_number()) {
-        property_out.set_int64_value(json_obj.get<int64_t>());
-      }
-      break;
-    }
-    case RedfishProperty::DATE_TIME_OFFSET: {
-      absl::Time timevalue;
-      if (!json_obj.is_string()) {
+  if (property.property_element_type() ==
+      RedfishProperty::COLLECTION_PRIMITIVE) {
+    ECCLESIA_RETURN_IF_ERROR(GetCollectionPropertyFromRedfishObject(
+        redfish_object, property, json_obj, property_out));
+  } else {
+    // clients to use new property value fields.
+    switch (property.type()) {
+      case RedfishProperty::STRING: {
+        if (json_obj.is_string()) {
+          property_out.set_string_value(json_obj.get<std::string>());
+        }
         break;
       }
-      if (absl::ParseTime("%Y-%m-%dT%H:%M:%S%Z", json_obj.get<std::string>(),
-                          &timevalue, nullptr)) {
-        absl::StatusOr<google::protobuf::Timestamp> timestamp =
-            AbslTimeToProtoTime(timevalue);
-        if (timestamp.ok()) {
-          *property_out.mutable_timestamp_value() = std::move(*timestamp);
+      case RedfishProperty::BOOLEAN: {
+        if (json_obj.is_boolean()) {
+          property_out.set_boolean_value(json_obj.get<bool>());
         }
+        break;
       }
-      break;
-    }
-    default: {
-      break;
+      case RedfishProperty::DOUBLE: {
+        if (json_obj.is_number()) {
+          property_out.set_double_value(json_obj.get<double>());
+        }
+        break;
+      }
+      case RedfishProperty::INT64: {
+        if (json_obj.is_number()) {
+          property_out.set_int64_value(json_obj.get<int64_t>());
+        }
+        break;
+      }
+      case RedfishProperty::DATE_TIME_OFFSET: {
+        absl::Time timevalue;
+        if (!json_obj.is_string()) {
+          break;
+        }
+        if (absl::ParseTime("%Y-%m-%dT%H:%M:%S%Z", json_obj.get<std::string>(),
+                            &timevalue, nullptr)) {
+          absl::StatusOr<google::protobuf::Timestamp> timestamp =
+              AbslTimeToProtoTime(timevalue);
+          if (timestamp.ok()) {
+            *property_out.mutable_timestamp_value() = std::move(*timestamp);
+          }
+        }
+        break;
+      }
+      default: {
+        break;
+      }
     }
   }
-  if (!property_out.value_case()) {
+  if (!(property_out.value_case() || property_out.has_primitive_value() ||
+        property_out.has_collection_value())) {
     return absl::InvalidArgumentError("Invalid property type");
   }
   return property_out;
